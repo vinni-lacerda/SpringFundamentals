c0688c74607f56fff65ec4c6ce9031ac8a942ca0
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-14 22:39:11 -0300

    adding a way to use sum by endpoint

--- src/main/java/vinilab/springFundamentals/controllers/MathController.java
+++ src/main/java/vinilab/springFundamentals/controllers/MathController.java
@@ -8,7 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo){
-        return 1D;
+    public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
+        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        }
+    public static double convertToDouble(String strNumber) throws Exception {
+        if(strNumber == null || strNumber.isEmpty()) throw new Exception();
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean IsNumeric(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
     }
 }
