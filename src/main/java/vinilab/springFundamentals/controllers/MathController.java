43e3dff70461ed112f3b78e06bf0fee30b4e87d3
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-14 23:11:28 -0300

    adding exceptions to handle errors

--- src/main/java/vinilab/springFundamentals/controllers/MathController.java
+++ src/main/java/vinilab/springFundamentals/controllers/MathController.java
@@ -3,17 +3,19 @@ package vinilab.springFundamentals.controllers;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vinilab.springFundamentals.exception.UnsupportedMathOperationException;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo)) throw new IllegalArgumentException();
+        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo))
+            throw new UnsupportedMathOperationException("Please set a numeric value");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
         }
     public static double convertToDouble(String strNumber) throws Exception {
-        if(strNumber == null || strNumber.isEmpty()) throw new Exception();
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");;
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
