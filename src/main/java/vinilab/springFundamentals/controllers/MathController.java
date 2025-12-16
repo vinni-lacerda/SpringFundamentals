0808dc7d70a422ce62fa616ba81450fc5b60ccd1
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-15 23:51:53 -0300

    Refactoring to a cleaner version

--- src/main/java/vinilab/springFundamentals/controllers/MathController.java
+++ src/main/java/vinilab/springFundamentals/controllers/MathController.java
@@ -4,50 +4,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vinilab.springFundamentals.exception.UnsupportedMathOperationException;
+import vinilab.springFundamentals.math.SimpleMath;
+import vinilab.springFundamentals.request.converters.NumberConverter;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
+
+    private SimpleMath math = new SimpleMath();
+
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo))
+        if(!NumberConverter.IsNumeric(numberOne) || !NumberConverter.IsNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
         }
-    public static double convertToDouble(String strNumber) throws Exception {
-        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");;
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-    }
 
-    public static boolean IsNumeric(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");
-        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
-    }
     @RequestMapping("/subtract/{numberOne}/{numberTwo}")
     public Double subtract(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo))
+        if(!NumberConverter.IsNumeric(numberOne) || !NumberConverter.IsNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return math.subtract(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
     @RequestMapping("/multi/{numberOne}/{numberTwo}")
     public Double multiply(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo))
+        if(!NumberConverter.IsNumeric(numberOne) || !NumberConverter.IsNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return math.multiply(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
     @RequestMapping("/divide/{numberOne}/{numberTwo}")
     public Double divide(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!IsNumeric(numberOne) || !IsNumeric(numberTwo))
+        if(!NumberConverter.IsNumeric(numberOne) || !NumberConverter.IsNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return math.divide(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
     @RequestMapping("/sqrt/{numberOne}")
     public Double sqrRoot(@PathVariable String numberOne) throws Exception {
-        if(!IsNumeric(numberOne))
+        if(!NumberConverter.IsNumeric(numberOne))
             throw new UnsupportedMathOperationException("Please set a numeric value");
-        Double result = Math.sqrt(convertToDouble(numberOne));
+        Double result = Math.sqrt(NumberConverter.convertToDouble(numberOne));
         return result;
     }
 }
