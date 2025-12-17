2110e6e47ab59a76e948b51d946d982a78270067
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-16 23:47:55 -0300

    Person services fixed

--- injecaodependencia/src/main/java/vinilab/springFundamentals/request/converters/PersonServices.java
+++ injecaodependencia/src/main/java/vinilab/springFundamentals/request/converters/PersonServices.java
@@ -1,10 +1,25 @@
 package vinilab.springFundamentals.request.converters;
 
 import org.springframework.stereotype.Service;
+import vinilab.springFundamentals.model.Person;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
-    private AtomicLong
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id){
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Vinicius");
+        person.setLastName("Lacerda");
+        person.setAdress("Salvador, Bahia, Brasil");
+        person.setGender("Male");
+        return person;
+    }
 }
