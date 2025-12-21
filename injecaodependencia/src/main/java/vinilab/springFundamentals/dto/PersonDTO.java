4a030bd029be3a4ebe87cd46858f27f8119a68e7
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-21 18:22:07 -0300

    feat: ObjectMapper implemented.

---
+++ injecaodependencia/src/main/java/vinilab/springFundamentals/dto/PersonDTO.java
@@ -0,0 +1,87 @@
+package vinilab.springFundamentals.dto;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "person")
+public class PersonDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
+    private String lastName;
+    @Column(nullable = false, length = 100)
+    private String adress;
+    @Column(nullable = false, length = 6)
+    private String gender;
+
+    public PersonDTO() {
+    }
+
+    public PersonDTO(Long id, String firstName, String lastName, String adress, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(adress, person.adress) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, adress, gender);
+    }
+}
