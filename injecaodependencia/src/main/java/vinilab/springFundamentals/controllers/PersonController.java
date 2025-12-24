5cc1aebf12cdc7d7354dfb0f2b297197571fe262
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-24 02:44:45 -0300

    refactor: refactoring person controller to use PersonDTO instead of Person

--- injecaodependencia/src/main/java/vinilab/springFundamentals/controllers/PersonController.java
+++ injecaodependencia/src/main/java/vinilab/springFundamentals/controllers/PersonController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vinilab.springFundamentals.model.Person;
+import vinilab.springFundamentals.dto.PersonDTO;
 import vinilab.springFundamentals.services.PersonServices;
 
 import java.util.List;
@@ -17,21 +17,21 @@ public class PersonController {
     private PersonServices personServices;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         return personServices.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") Long id){
+    public PersonDTO findById(@PathVariable("id") Long id){
         return personServices.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person){
+    public PersonDTO create(@RequestBody PersonDTO person){
         return personServices.create(person);
     }
     @PutMapping
-    public Person update(@RequestBody Person person){
+    public PersonDTO update(@RequestBody PersonDTO person){
         return personServices.update(person);
     }
 
