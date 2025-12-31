1e215b6ad4f72bd56ad978e59c51f20b96e5a0c2
vinni-lacerda <vini.lacerda@hotmail.com>
2025-12-30 21:39:43 -0300

    feat(db): create initial database schema

---
+++ injecaodependencia/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` VALUES
+(1,'Ayrton','Senna','Santos - Brazil','Male'),
+(2,'Bruna','Jezler','Bahia - Brasil','Female'),
+(3,'Bianca','Jezler','Bahia - Brasil', 'Female'),
+(4,'Muhammed','Ali','Kentucky - USA','Male'),
+(5,'Nelson','Mandela','Mvezo - South Africa','Male'),
+(6,'Nikola','Tesla','Smiljan - Croatia','Male');
